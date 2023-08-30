5548df12198d58e511eb498be805d5f4404b1f95
public-release <as-public-release@janestreet.com>
2023-08-30 10:23:16 +0100

    v0.17~preview.128.37+01

--- src/ppx_here.ml
+++ src/ppx_here.ml
@@ -1,12 +1,11 @@
 open Ppxlib
 
 let here =
-  Extension.declare "here" Extension.Context.expression
+  Extension.declare
+    "here"
+    Extension.Context.expression
     Ast_pattern.(pstr nil)
     (fun ~loc ~path:_ -> Ppx_here_expander.lift_position ~loc)
 ;;
 
-let () =
-  Driver.register_transformation "here"
-    ~extensions:[ here ]
-;;
+let () = Driver.register_transformation "here" ~extensions:[ here ]
